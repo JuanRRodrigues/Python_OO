9625635bf94ee27947678e6c8b48bb3c15844bb8
Juan R. Rodrigues <juancontato27@gmail.com>
2024-01-28 20:13:18 -0300

    Static-Test

--- account.py
+++ account.py
@@ -14,8 +14,14 @@ class account:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
+
     def saca(self,valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
 
     def transfere(self, valor, destino):
         self.saca(valor)
